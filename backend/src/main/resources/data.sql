6c5e6d361da499b8c267b6dc40f1b525017b8e38
Jonas Ribeiro <jonasribeiro41@yahoo.com.br>
2021-09-27 17:52:59 -0300

    Section

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -18,6 +18,11 @@ INSERT INTO tb_course (name, img_Uri, img_Gray_Uri) VALUES ('BootCamp HTML', 'ht
 INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES ('1.0', TIMESTAMP WITH TIME ZONE '2020-11-13T03:00:00Z', TIMESTAMP WITH TIME ZONE '2021-11-13T03:00:00Z', 1);
 INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES ('2.0', TIMESTAMP WITH TIME ZONE '2020-12-13T03:00:00Z', TIMESTAMP WITH TIME ZONE '2021-12-13T03:00:00Z', 1);
 
-INSERT INTO  tb_resource (title, description, position, img_Uri, type, offer_id) VALUES ('Trilha HTML', 'Trilha pricipal do curso', 1, 'https://c.pxhere.com/images/3a/c9/b759c4df41afe66007012bf12d6f-1437855.jpg!d', 1, 1);
-INSERT INTO  tb_resource (title, description, position, img_Uri, type, offer_id) VALUES ('Forum', 'Tire suas dúvidas', 2, 'https://c.pxhere.com/images/3a/c9/b759c4df41afe66007012bf12d6f-1437855.jpg!d', 2, 1);
-INSERT INTO  tb_resource (title, description, position, img_Uri, type, offer_id) VALUES ('Lives', 'Lives exclusivas para a turma', 3, 'https://c.pxhere.com/images/3a/c9/b759c4df41afe66007012bf12d6f-1437855.jpg!d', 0, 1);
+INSERT INTO tb_resource (title, description, position, img_Uri, type, offer_id) VALUES ('Trilha HTML', 'Trilha pricipal do curso', 1, 'https://c.pxhere.com/images/3a/c9/b759c4df41afe66007012bf12d6f-1437855.jpg!d', 1, 1);
+INSERT INTO tb_resource (title, description, position, img_Uri, type, offer_id) VALUES ('Forum', 'Tire suas dúvidas', 2, 'https://c.pxhere.com/images/3a/c9/b759c4df41afe66007012bf12d6f-1437855.jpg!d', 2, 1);
+INSERT INTO tb_resource (title, description, position, img_Uri, type, offer_id) VALUES ('Lives', 'Lives exclusivas para a turma', 3, 'https://c.pxhere.com/images/3a/c9/b759c4df41afe66007012bf12d6f-1437855.jpg!d', 0, 1);
+
+INSERT INTO tb_section (title, description, position, img_Uri, resource_id, prerequisite_id) VALUES ('Capítulo 1', 'Neste capítulo vamos começar', 1, 'https://c.pxhere.com/images/3a/c9/b759c4df41afe66007012bf12d6f-1437855.jpg!d', 1, null);
+INSERT INTO tb_section (title, description, position, img_Uri, resource_id, prerequisite_id) VALUES ('Capítulo 2', 'Neste capítulo vamos continuar', 2, 'https://c.pxhere.com/images/3a/c9/b759c4df41afe66007012bf12d6f-1437855.jpg!d', 1, 1);
+INSERT INTO tb_section (title, description, position, img_Uri, resource_id, prerequisite_id) VALUES ('Capítulo 3', 'Neste capítulo vamos finalizar', 3, 'https://c.pxhere.com/images/3a/c9/b759c4df41afe66007012bf12d6f-1437855.jpg!d', 1, 2);
+
