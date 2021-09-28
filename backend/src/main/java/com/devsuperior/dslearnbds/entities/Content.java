c0fa60190d2953013f2ba6ad256a5effb9e86b30
Jonas Ribeiro <jonasribeiro41@yahoo.com.br>
2021-09-28 16:55:32 -0300

    Lesson, content, task

---
+++ backend/src/main/java/com/devsuperior/dslearnbds/entities/Content.java
@@ -0,0 +1,38 @@
+package com.devsuperior.dslearnbds.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_content")
+public class Content extends Lesson {
+	private static final long serialVersionUID = 1L;
+
+	private String textContent;
+	private String VideoUri;
+	
+	public Content() {
+	}
+
+	public Content(Long id, String title, Integer position, Section section, String textContent, String videoUri) {
+		super(id, title, position, section);
+		this.textContent = textContent;
+		VideoUri = videoUri;
+	}
+
+	public String getTextContent() {
+		return textContent;
+	}
+
+	public void setTextContent(String textContent) {
+		this.textContent = textContent;
+	}
+
+	public String getVideoUri() {
+		return VideoUri;
+	}
+
+	public void setVideoUri(String videoUri) {
+		VideoUri = videoUri;
+	}
+}
